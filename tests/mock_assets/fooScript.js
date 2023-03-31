b471e2010a8e42ffe1e559e1a397f482d2579a25
Daniel Feichtinger <daniel@auxilit.com>
2023-03-31 14:48:29 +0100

    Remove debug code

--- tests/mock_assets/fooScript.js
+++ tests/mock_assets/fooScript.js
@@ -1,11 +1,10 @@
+/* eslint-disable no-undef */
 hypermap.set('foo', 'bar');
 
 let counter = 0;
 hypermap.addEventListener('changed', (event) => {
-  console.log('changed!', event.detail.key);
   if (event.detail.key === 'input') {
     counter += 1;
     hypermap.set('output', counter);
   }
 });
-
